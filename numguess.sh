97cf90b44b092b549a55160a03e723d7827d9c33
Konstantin Läufer <laufer@cs.luc.edu>
2023-11-30 17:39:15 -0600

    initial import
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

---
+++ numguess.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# use first argument as number
+# or pick one at random
+
+NUMBER=$1
+if [ -z "$1" ]; then
+    NUMBER=$(shuf -i 1-100 -n 1)
+fi
+
+echo number is $NUMBER
+
+read -p "What's your guess? " GUESS
+
+# TODO check validity of input
+
+while [ "$GUESS" -ne "$NUMBER" ]; do
+
+    # TODO feedback to user - too low/high
+
+    # TODO count number of steps -
+    # terminate if max exceeded
+    
+    echo "Try again"
+    read -p "What's your guess? " GUESS
+
+done
+
+echo "You got it"
