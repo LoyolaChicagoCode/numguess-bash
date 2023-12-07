4ee9433b94e014b7182362cb0102caa85a097c90
Konstantin Läufer <laufer@cs.luc.edu>
2023-12-07 13:12:16 -0600

    finished game
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- numguess.sh
+++ numguess.sh
@@ -1,29 +1,52 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+steps=0
+max_steps=10
+max_number=100
 
 # use first argument as number
 # or pick one at random
 
-NUMBER=$1
+number=$1
 if [ -z "$1" ]; then
-    NUMBER=$(shuf -i 1-100 -n 1)
+    number=$(shuf -i 1-$max_number -n 1)
 fi
 
-echo number is $NUMBER
+# TODO comment out after testing
 
-read -p "What's your guess? " GUESS
+echo "DEBUG: Number is $number"
 
-# TODO check validity of input
+# multi-line output (with variable subsitution)
 
-while [ "$GUESS" -ne "$NUMBER" ]; do
+cat <<EOF
+Hi and welcome to the number guessing game!
+I've made up a number between 0 and $max_number
+Your job is to guess this number within at most $max_steps steps.
+Good luck!
+EOF
 
-    # TODO feedback to user - too low/high
+while true; do
 
-    # TODO count number of steps -
-    # terminate if max exceeded
+    step=$((step+1))
+
+    # terminate if max number of steps exceeded
+
+    if [ $step -gt $max_steps ]; then
+	echo "Sorry, that's more than $max_steps steps!"
+	exit
+    fi
     
-    echo "Try again"
-    read -p "What's your guess? " GUESS
+    # check validity of input and give feedback
 
+    read -p "STEP $step: What's your guess? " guess
+    if [[ ! $guess =~ ^[0-9]+$ ]]; then
+	echo "Your input $guess isn't a valid number"
+    elif [ $guess -lt $number ]; then
+	echo "Oops, that was too low."
+    elif [ $guess -gt $number ]; then
+	echo "Oops, that was too high."
+    else
+	echo "Congratulations, you got it after only $step step(s)!"
+	exit
+    fi
 done
-
-echo "You got it"
